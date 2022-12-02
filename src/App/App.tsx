319005ed5d975ffacdd0996f42b394ec724c7d6d
amfunNyLORD <funnybulletgames@yandex.ru>
2022-12-02 10:31:24 +0300

    Поменял структуру проекта, изменил расширения с js на tsx, изменил расширение иконки

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MenuBar from "../Components/MenuBar/MenuBar"
+import React from 'react';
+
+function App() {
+  return (
+      <div>
+        <MenuBar />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MenuBar/MenuBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './MenuBar.module.css';
+
+export function MenuBar() {
+    return (
+        <div className={style.header}>
+            <div className={style.header__icon}></div>
+            <div className={style.header__input}>
+                <span className={style.header__input__text}>Новая презентация</span>
+            </div>
+            <div className={style.header__action}>
+                <div className={style.header__action__text}>Создать</div>
+                <div className={style.header__action__text}>Открыть</div>
+                <div className={style.header__action__text}>Сохранить</div>
+            </div>
+        </div>
+    );
+}
+
+export default MenuBar;
