d5d159a65b87e77947ec41ce304c3a7b70253c29
Daniil <koldyrev98@icloud.com>
2022-12-06 12:19:41 +0300

    Сделал новый блок WorkSpace, убрал скроллинг

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,20 @@
-import './App.css';
+import './App.module.css';
+import style from './App.module.css';
 import React from 'react';
 import MenuBar from "../Components/MenuBar/MenuBar"
 import ToolBar from "../Components/ToolBar/ToolBar";
 import SlideList from "../Components/SlideList/SlideList";
+import WorkSpace from "../Components/WorkSpace/WorkSpace";
 
 function App() {
   return (
       <div>
           <MenuBar />
           <ToolBar />
-          <SlideList />
+          <div className={style.content}>
+            <SlideList />
+            <WorkSpace />
+          </div>
       </div>
   );
 }
---
+++ src/Components/WorkSpace/WorkSpace.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './WorkSpace.module.css';
+
+export function WorkSpace()
+{
+    return (
+        <div className={style.workspace__background}>
+            <div className={style.workspace__content}>
+
+            </div>
+        </div>
+    );
+}
+
+export default WorkSpace;
