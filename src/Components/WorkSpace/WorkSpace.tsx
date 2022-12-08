102905bbcff460c5415e5170c60abcb3e6ff5dad
amfunNyLORD <funnybulletgames@yandex.ru>
2022-12-08 19:21:52 +0300

    Убрал марджины и падинги у body, добавил картинку, добавил отрисовку примитивов, текста и картинки

--- src/Components/WorkSpace/WorkSpace.tsx
+++ src/Components/WorkSpace/WorkSpace.tsx
@@ -6,7 +6,11 @@ export function WorkSpace()
     return (
         <div className={style.workspace__background}>
             <div className={style.workspace__content}>
-
+                <span className={style.workspace__content__text}>Тестовый текст</span>
+                <div className={style.workspace__content__square}></div>
+                <div className={style.workspace__content__triangle}></div>
+                <div className={style.workspace__content__circle}></div>
+                <img className={style.workspace__content__picture} src={require('../../images/priora.jpeg')} alt={'Priora'} />
             </div>
         </div>
     );
