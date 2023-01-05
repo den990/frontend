7acad0982a1a74d4a8a84d79f814b897aed6b681
Vadim <iskolen@mail.ru>
2023-01-05 14:25:26 +0300

    Сделал превью слайда(Изменяется пока только цвет)

--- src/Components/Slide/MiniSlide.tsx
+++ src/Components/Slide/MiniSlide.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import style from "./MiniSlide.module.css";
 import {selectSlideHandler} from "../../stateManager/stateManagerFunctions";
 
-const MiniSlide = (Props: {slideIndex: number, selected: boolean}) => {
+const MiniSlide = (Props: {slideIndex: number, selected: boolean, background: string}) => {
+    let styleContainer = {
+        background: Props.background
+    }
     return (
         <div key={Props.slideIndex} className={style.miniSlide}>
             <span className={Props.slideIndex < 10 ? style.miniSlide__index : Props.selected ? style.miniSlide__index_doubleDigits : style.miniSlide__index}>{Props.slideIndex}</span>
             <div className={Props.selected ? style.miniSlide__borders : undefined}>
-                <div onClick={(e) => selectSlideHandler(Props.slideIndex - 1, e, Props.selected)} className={style.miniSlide__container}></div>
+                <div onClick={(e) => selectSlideHandler(Props.slideIndex - 1, e, Props.selected)} style={styleContainer} className={style.miniSlide__container}></div>
             </div>
         </div>
     );
--- src/Components/SlideList/SlideList.tsx
+++ src/Components/SlideList/SlideList.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import style from './SlideList.module.css';
 import MiniSlide from "../Slide/MiniSlide";
 
-export function SlideList( Props: { slideList: Slide[], selectedSlides: Slide[] }) {
+export function SlideList( Props: { slideList: Slide[], selectedSlides: Slide[]}) {
     const slides = Props.slideList.map((slide, index) => (
-        <MiniSlide key={index} slideIndex={slide.slideIndex} selected={Props.selectedSlides.some((Slide) => Slide.slideIndex === slide.slideIndex)} />
+        <MiniSlide 
+            key={index} 
+            slideIndex={slide.slideIndex} 
+            selected={Props.selectedSlides.some((Slide) => Slide.slideIndex === slide.slideIndex)}
+            background={slide.background.code} />
     ));
     return (
         <div className={style.slideList}>
